7ad796b271c4497402225704ab975604d1fee106
Diego Cristian Alonso DHL ExpAM <diego.alonso@dhl.com>
2026-03-30 14:34:49 -0300

    503 error

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const strapi = require('@strapi/strapi');
 
-// Iniciar Strapi desde la carpeta 'dist' que ya contiene el build de producción
-strapi({ distDir: './dist' }).start();
+console.log('--- Iniciando Strapi (Modo Producción) ---');
+console.log('Cargando desde el directorio: ./dist');
+
+strapi({ distDir: './dist' })
+  .start()
+  .then(() => {
+    console.log('>>> Strapi está listo y escuchando en el puerto:', process.env.PORT || 1337);
+  })
+  .catch((error) => {
+    console.error('!!! Error fatal al iniciar Strapi:', error);
+    process.exit(1);
+  });
