c233553c25f77a1388010c3f32238eb3dd60ac51
Diego Cristian Alonso DHL ExpAM <diego.alonso@dhl.com>
2026-04-02 19:47:13 -0300

    Update plugins.ts

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -2,22 +2,24 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: 'local', // DESACTIVADO TEMPORALMENTE PARA EVITAR CRASHES EN RESTAURACIÓN
-      /* 
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env('CLOUDINARY_NAME', 'dg0msu8ru'),
+        api_key: env('CLOUDINARY_KEY', '517598867933172'),
+        api_secret: env('CLOUDINARY_SECRET', '6frfOJz7L7V_x-GEjttlfkibYRQ'),
       },
       actionOptions: {
         upload: {},
         uploadStream: {
-           breakpoints: false // No generar tamaños extra para evitar 502
+           breakpoints: false 
         },
         delete: {},
       },
-      */
+    },
+  },
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET', 'a_very_secure_random_secret_fallback_123'),
     },
   },
 });
