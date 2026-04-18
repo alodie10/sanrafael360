f7221c34c33ea9faf5b7f5347565588c51114ffe
alodie10 <alodie10@users.noreply.github.com>
2026-04-18 19:36:35 -0300

    12

--- backend/src/api/consulta-soporte/routes/consulta-soporte.ts
+++
@@ -1,9 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::consulta-soporte.consulta-soporte' as any, {
-  config: {
-    create: {
-      middlewares: ['api::consulta-soporte.consulta-validator'],
-    },
-  },
-});
--- backend/src/api/consulta-soporte/services/consulta-soporte.ts
+++
@@ -1,3 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::consulta-soporte.consulta-soporte' as any);
