376d04febd8c0c789836be261f0402a91613ea01
alodie10 <alodie10@users.noreply.github.com>
2026-04-18 14:47:45 -0300

    Update negocio.ts

--- backend/src/api/negocio/services/negocio.ts
+++ backend/src/api/negocio/services/negocio.ts
@@ -91,9 +91,9 @@ export default factories.createCoreService('api::negocio.negocio', ({ strapi }) 
 
     const updated = await repo.update(id, updateData);
     if (files) {
-      if (files.logo) await repo.uploadFile(updated.id, 'logo', files.logo);
-      if (files.imagen_portada) await repo.uploadFile(updated.id, 'imagen_portada', files.imagen_portada);
-      if (files.galeria) await repo.uploadFile(updated.id, 'galeria', files.galeria, true);
+      if (files.logo) await repo.uploadFile(updated.documentId, 'logo', files.logo);
+      if (files.imagen_portada) await repo.uploadFile(updated.documentId, 'imagen_portada', files.imagen_portada);
+      if (files.galeria) await repo.uploadFile(updated.documentId, 'galeria', files.galeria, true);
     }
     await repo.publish(id);
     return updated;
