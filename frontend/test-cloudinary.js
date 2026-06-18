c66a447a4f19711a4988b3e35510dc2055c36eb2
alodie10 <alodie10@users.noreply.github.com>
2026-06-18 18:19:48 -0300

    3

--- frontend/test-cloudinary.js
+++
@@ -1,4 +0,0 @@
-const url = "https://res.cloudinary.com/demo/video/upload/v1234/dog.mp4";
-const transformations = "f_webp,fl_awebp,q_auto,c_fill,w_400,h_400";
-const parts = url.split('/upload/');
-console.log(`${parts[0]}/upload/${transformations}/${parts[1]}`);
