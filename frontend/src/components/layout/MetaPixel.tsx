0ed77d861de5d78d76f496769871430681eb4b31
alodie10 <alodie10@users.noreply.github.com>
2026-06-22 11:45:12 -0300

    Update MetaPixel.tsx

--- frontend/src/components/layout/MetaPixel.tsx
+++ frontend/src/components/layout/MetaPixel.tsx
@@ -4,7 +4,7 @@ import Script from "next/script";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
-const FB_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
+const FB_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || "717295573214072";
 
 export const pageview = () => {
   if (typeof window !== "undefined" && (window as any).fbq) {
