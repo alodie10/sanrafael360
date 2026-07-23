d81cd90ff0cb2230389c3b13dff7c615cfa68d3b
alodie10 <alodie10@users.noreply.github.com>
2026-07-23 18:44:35 -0300

    Arreglo de reseñas sincro

---
+++ SR360 - Google Play package/_source_build/source/app/src/main/java/com/sanrafael360/www/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.sanrafael360.www.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
